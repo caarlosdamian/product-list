8f5f27759a17e36583128f9ba8211d3a7ba2586c
caarlosdamian <dapecarlos@gmail.com>
2026-02-10 19:43:07 -0600

    feat(style-guide): added styles guiden

---
+++ src/pages/main/Main.tsx
@@ -0,0 +1,9 @@
+export const Main = () => {
+  return (
+    <div>
+      <h1 className="font-red-hat">Testing</h1>
+      <h1 className="">Testing</h1>
+      <h1 className="font-red-hat-italic">Testing</h1>
+    </div>
+  );
+};
